713bc14b61a5d6b5a0fa4031edf5c5eb3ee5a625
SiJiL82 <sven.lowry@gmail.com>
2023-09-30 20:33:57 +0100

    Remove ready func from astronaut

--- scripts/Astronaut.gd
+++ scripts/Astronaut.gd
@@ -6,12 +6,5 @@ extends Node3D
 var rotate_direction = Vector3(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1)).normalized()
 var rotate_speed = randf_range(min_rotate_speed, max_rotate_speed)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var x = randf_range(0, TAU)
-	var y = randf_range(0, TAU)
-	var z = randf_range(0, TAU)
-	rotation = Vector3(x, y, z)
-
 func _process(_delta):
 	rotate_object_local(rotate_direction, rotate_speed)
