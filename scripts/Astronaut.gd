a6d3718c98a953057d1470266ea2d6fd4369c4c5
SiJiL82 <sven.lowry@gmail.com>
2023-10-01 18:41:05 +0100

    Add astronauts to group on spawn

--- scripts/Astronaut.gd
+++ scripts/Astronaut.gd
@@ -1,10 +1,14 @@
 extends Node3D
+class_name Astronaut
 
 @export var min_rotate_speed: float = 0.001
 @export var max_rotate_speed: float = 0.03
 
 var rotate_direction = Vector3(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1)).normalized()
 var rotate_speed = randf_range(min_rotate_speed, max_rotate_speed)
+
+func _ready():
+	add_to_group("Astronauts")
 
 func _process(_delta):
 	rotate_object_local(rotate_direction, rotate_speed)
